a82de209c6b5c330f8f5139410bfbd68cd1f7b39
jl945 <519414839@qq.com>
2015-04-23 15:07:04 +0800

    api 配置

--- common/config/params.php
+++ common/config/params.php
@@ -1,18 +1,15 @@
 <?php
 return [
-
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
 
-    //验证码 极验验证
-    'geetest_captcha_id'=>'b3a60a5dd8727fe814b43fce2ec7412a',
-    'geetest_captcha_key'=>'070346b26995c62043f2cc68a8fe3527',
+    'imConfig'=>array(
+        'client_id'=>'YXA6KyC-AN2REeSfRjnHRAcEug',
+        'client_secret'=>'YXA6xhx9l405mEntiftss1inwfk8zNY',
+        'org_name'=>'suiuu',
+        'app_name'=>'suiuu'
+    ),
 
-    //云OSS
-    'yun_oss_key_id'=>'LaKLZHyL2Dmy8Qqq',
-    'yun_oss_key_secret'=>'c7xPteQRqjV8nNB8xGFIZoFijzjDLX',
-    'yun_oss_host_image'=>'image.suiuu.com',
-
-
+    'apiPassword'=>'sui@uu~9527&*'
 ];
